11bbe98129eee7a0d10a8da0e8e8dcf18c9a1688
Manish Dange <dangemanish35@gmail.com>
2026-03-07 21:12:33 +0530

    PostgreSQL database connected

--- Backend/fullbackend/fullbackend/settings.py
+++ Backend/fullbackend/fullbackend/settings.py
@@ -79,11 +79,25 @@ WSGI_APPLICATION = 'fullbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        "postgresql://django_user:tJh4Ljh5rPnabOrmQYryLBNZ9cLN566W@dpg-d6m4a4fkijhs73fpkh2g-a.singapore-postgres.render.com/django_db_zwyt"
+    )
+}
+
+
+DATABASES = {
+ 'default': {
+ 'ENGINE': 'django.db.backends.postgresql',
+ 'NAME': 'django_db',
+ 'USER': 'postgres',
+ 'PASSWORD': 'Dange@098',
+ 'HOST': 'localhost',
+ 'PORT': '5432',
+ }
 }
 
 
